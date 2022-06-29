02e27bf5f5aec46130dc4918eb58607a7b47485e
Foster0x <sfoster2010.sf@gmail.com>
2022-06-29 15:20:23 -0400

    Add project files.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a numer in between 0 and 100: ");
+                string? s = Console.ReadLine();
+
+                int i = int.Parse(s!);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("To high! Guess lower...");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("To low! Guess higher...");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("YOU WIN!");
+                    win = true;
+                }
+
+                Console.WriteLine();
+
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing the game!");
+        }
+    }
+}
